9e86f9c3552a886280775258689002b419f4fa48
fantasma369to <afiliadoudemy2019@gmail.com>
2025-07-16 08:01:17 -0500

    supabase variables corregidas

--- src/config/supabaseAlejandria.js
+++ src/config/supabaseAlejandria.js
@@ -1,7 +1,11 @@
 import { createClient } from "@supabase/supabase-js";
-import "dotenv/config";
-const supabaseAlejandria = createClient(
-  process.env.SUPABASE_URL,
-  process.env.SUPABASE_SERVICE_ROLE_KEY
-);
+
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_SERVICE_ROLE_KEY;
+
+if (!supabaseUrl || !supabaseKey) {
+  throw new Error("Missing Supabase environment variables.");
+}
+
+const supabaseAlejandria = createClient(supabaseUrl, supabaseKey);
 export default supabaseAlejandria;
